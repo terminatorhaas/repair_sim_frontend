cf3405d1761b5a6bf047c4ab615d9d3fd9d21cba
Moritz Moser <moritz.moser@mobisys.de>
2021-07-01 12:32:48 +0200

    Welcome Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
       children: [
         {
           path: '',
-          component: HomeComponent
+          component: WelcomeComponent
         },
         {
           path: 'calender',
